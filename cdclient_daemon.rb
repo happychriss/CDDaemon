1b6b36780f83f585afb08dae822ff5e5dd96ba3a
happychriss <pmsfriend@googlemail.com>
2015-02-28 21:38:09 +0100

    first working solution with green and yellow status lights

--- cdclient_daemon.rb
+++ cdclient_daemon.rb
@@ -26,101 +26,105 @@ def terminate(options, web_server_uri)
 end
 
 
-
 # Tries several times to connect to the webserver
-def connect_to_webserver(drb_uri, options, web_server_uri,services,reply)
-  try_counter=0; try_max=10
+def connect_to_webserver(drb_uri, options, web_server_uri)
+  try_counter=0; try_max= 2
 
-  loop do
-    begin
-      puts "#{Time.now} *** try connecting to : #{drb_uri}"
-      sleep(rand*5)
-      RestClient.post web_server_uri+'/connectors', {:connector => {:service => options[:service], :uri => drb_uri, :uid => options[:uid], :prio => options[:prio]}}, :content_type => :json, :accept => :json
-      puts "*** connection succesfully established"
-      return true
-    rescue => e
-      try_counter=try_counter+1
-      puts "Failed with error:#{e.message} and try number:#{try_counter}"
+      loop do
+        begin
+          puts "#{Time.now} *** try connecting to : #{drb_uri}"
+          sleep(rand*5)
+          RestClient.post web_server_uri+'/connectors', {:connector => {:service => options[:service], :uri => drb_uri, :uid => options[:uid], :prio => options[:prio]}}, :content_type => :json, :accept => :json
+          puts "*** connection succesfully established"
+          return true
+        rescue => e
+          try_counter=try_counter+1
+          puts "Failed with error:#{e.message} and try number:#{try_counter}"
 
-      if try_counter==try_max
-        services.delete(reply.fullname)
-        DRb.stop_service
-        puts "Disconnected from Service: #{reply.fullname}"
-        return false
+          if try_counter==try_max
+            puts "Disconnected from Service: #{web_server_uri}"
+            return false
+          end
+
+        end
       end
-
-    end
-  end
 end
 
 def run_drb_daemons(options)
 
 
   browser = DNSSD::Service.new
-  services = {}
   web_server_uri=''
-  drb_uri="druby://#{Socket.gethostname}:#{options[:port]}"   ## Create uri of drb-service that is the current host and the port from the config file
+  drb_uri="druby://#{Socket.gethostname}:#{options[:port]}" ## Create uri of drb-service that is the current host and the port from the config file
   avahi_name="Cleandesk_#{options[:avahi_prefix]}" ## Create Avahi Service Name, this is the avahi service the daemon is looking for
+  service_obj=nil
+  connection_count=0
+  connected=false
 
   puts "****** Waiting for Service request avahi: #{avahi_name}"
 
   browser.browse '_cds._tcp' do |reply|
 
-    if reply.flags.add? then
+    if reply.name==avahi_name
 
-      puts "#{Time.now}: Found Service: #{reply.fullname}"
+      if reply.flags.add? then
 
-      if reply.name==avahi_name and services[reply.fullname].nil?
+        connection_count=connection_count+1
+        puts "#{Time.now}: Found Service: #{reply.fullname} - count: #{connection_count}"
 
-        services[reply.fullname] = reply
+        if connection_count==1 and not connected
 
-        ## Start DRB Service and connect to URI
-        DNSSD::Service.new.resolve reply do |r|
+          ## Start DRB Service and connect to URI
+          DNSSD::Service.new.resolve reply do |r|
 
-          ## Create the uri of the web-server to sent confirmation, read from the service request
-          web_server_uri="#{r.target}:#{r.port}"
+            ## Create the uri of the web-server to sent confirmation, read from the service request
+            web_server_uri="#{r.target}:#{r.port}"
 
-          #generate Service Object for DRB
-          service_obj=Object.const_get(options[:service]).new(web_server_uri, options)
+            if service_obj.nil? or not service_obj.alive?
 
-          ### Start DRB Service
-          puts "*** Providing service for: #{web_server_uri} via DRB: #{drb_uri} and  and subnet: #{options[:subnet]} ***"
+              #generate Service Object for DRB
+              service_obj=Object.const_get(options[:service]).new(web_server_uri, options)
 
-          acl = ACL.new(%W(deny all
+              ### Start DRB Service
+              puts "*** Start DRB  for: #{web_server_uri} via DRB: #{drb_uri} and  and subnet: #{options[:subnet]} ***"
+
+              acl = ACL.new(%W(deny all
                            allow #{options[:subnet]}.*
                            allow localhost))
 
-          DRb.install_acl(acl)
-          DRb.start_service(drb_uri, service_obj)
-          DRb.uri
+              DRb.install_acl(acl)
+              DRb.start_service(drb_uri, service_obj)
+              DRb.uri
 
-          ### Ancounce Service to Server by sending a post request, trying it several times, as avahi service may be up and running before web-server is ready
-          connect_to_webserver(drb_uri, options, web_server_uri,services,reply)
+            else
+              puts "*** DRB already available for: #{web_server_uri} via DRB: #{drb_uri} and  and subnet: #{options[:subnet]} ***"
+            end
 
-          break unless r.flags.more_coming?
+            ### Ancounce Service to Server by sending a post request, trying it several times, as avahi service may be up and running before web-server is ready
+            connected=connect_to_webserver(drb_uri, options, web_server_uri)
+
+            break unless r.flags.more_coming?
+
+          end
+
+          Thread.abort_on_exception = true
+          trap 'INT' do
+            terminate(options, web_server_uri)
+          end
+          trap 'TERM' do
+            terminate(options, web_server_uri)
+          end
 
         end
 
-        Thread.abort_on_exception = true
-        trap 'INT' do
-          terminate(options, web_server_uri)
-        end
-        trap 'TERM' do
-          terminate(options, web_server_uri)
-        end
+      else
+        connection_count=connection_count-1
+        puts "#{Time.now}: Lost Service: #{reply.fullname} - count: #{connection_count}"
 
-      end
-
-    else
-      puts "#{Time.now}: Lost Service: #{reply.fullname}"
-
-      if reply.name==avahi_name and not services[reply.fullname].nil?
-
-        ## check if server is down or just a short avahi problem (lost connection)
-        begin
-          puts "******* Lost CONFIGURED service for: #{reply.fullname}.. try reconnect to #{web_server_uri} *****************"
+        if connection_count==1 and connected
+          puts "#{Time.now} ******* Lost CONFIGURED service for: #{reply.fullname}.. try reconnect to #{web_server_uri} *****************"
           sleep(1) ### give the server some time, assuming the network was down for a second  and trie to reconnect
-          connect_to_webserver(drb_uri, options, web_server_uri,services,reply)
+          connected=connect_to_webserver(drb_uri, options, web_server_uri)
         end
 
       end
